59a1cfbb2b658f28cbe8dc82c88952c3bfb9767c
Amber Williams <amberswebdesign@gmail.com>
2018-05-14 16:51:51 -0500

    CR-Contact form edits
    Consumer research study contact form added State and Zipcode form fields

--- nscconsumerresearch/consumerResearch/CR-contact-script/process.php
+++ nscconsumerresearch/consumerResearch/CR-contact-script/process.php
@@ -6,6 +6,8 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $address = $_POST['address'];
+$state = $_POST['state'];
+$zipcode = $_POST['zipcode'];
 $from = "<" . $email . ">";
 $to = "amber@neriumskincare.com , egutierrez@neriumbiotech.com , whitemichael646@gmail.com";
 $subject = "Consumer Research Form Submission";
@@ -13,7 +15,9 @@ $body =
 "Full name: {$name}
 From: {$email}
 Phone: {$phone}
-Address: {$address}";
+Address: {$address}
+State: {$state}
+Zipcode: {$zipcode}";
 mail($to, "Consumer Research Form Submission", $body);
 header('Location: ../../thanks');
 }
